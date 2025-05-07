a2cbf42c8ed934b1d280a752b451a884abc8ce25
rimonppip0 <rimonppip0@gmail.com>
2025-05-07 07:10:26 +0000

    image slider by javascript

--- class_13/morning_session/js/script.js
+++ class_13/morning_session/js/script.js
@@ -2,5 +2,34 @@ var btnElement = document.getElementById("btn");
 
 btnElement.onclick = function () {
   document.getElementById("heading").innerText =
-    "National Institute of Youth Development";
+    "Currently Learning Javascript...";
+};
+
+var mainImage = document.getElementById("mainImage");
+var imgOne = document.getElementById("img1");
+
+imgOne.onclick = function () {
+  var imgUrl = imgOne.getAttribute("src");
+  mainImage.setAttribute("src", imgUrl);
+};
+
+var imgTwo = document.getElementById("img2");
+
+imgTwo.onclick = function () {
+  var imgUrl = imgTwo.getAttribute("src");
+  mainImage.setAttribute("src", imgUrl);
+};
+
+var imgThree = document.getElementById("img3");
+
+imgThree.onclick = function () {
+  var imgUrl = imgThree.getAttribute("src");
+  mainImage.setAttribute("src", imgUrl);
+};
+
+var imgFour = document.getElementById("img4");
+
+imgFour.onclick = function () {
+  var imgUrl = imgFour.getAttribute("src");
+  mainImage.setAttribute("src", imgUrl);
 };
